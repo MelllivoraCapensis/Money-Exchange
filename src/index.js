1eed8b80969aea4cd48ad65e28abc140e377ee6a
MelllivoraCapensis <__>
2018-09-16 23:43:42 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  if(currency<0) 
+  	return {};
+  if(currency>10000)
+  	return  {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  var nominals={'H':50,'Q':25,'D':10,'N':5,'P':1};
+  var result={};
+  var rest=currency;
+  for (var key in nominals) {
+  	var count=Math.floor(rest/nominals[key]);
+  	if(count>0)
+  		result[key]=count;
+  	rest=rest%nominals[key];
+   }
+   return result;
 }
